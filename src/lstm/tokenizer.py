e91dfbe6d1db383ea5ca5cdab4767fbdc4f24613
Robert Ferencsik <robert.ferencsik@bitninja.io>
2026-02-16 18:40:32 +0100

    Add methods and arguments for tokenizer class

---
+++ src/lstm/tokenizer.py
@@ -0,0 +1,20 @@
+
+class SentencePieceTokenizer:
+    def __init__(self, model_path):
+        pass
+
+    def encode(self, text):
+        pass
+    def decode(self, token_ids):
+        pass
+
+    def encode_batch(
+        self,
+        texts,
+        padding,
+        truncation,
+        return_lengths):
+        pass
+
+    def get_vocab_size(self):
+        pass
